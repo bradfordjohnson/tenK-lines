71737d46d10e47f369b7b66927634375adad1644
bradfordjohnson <bradfordljohnson21+github@gmail.com>
2024-03-09 00:17:17 -0500

    Add LineProgress class to count lines in files

---
+++ python/line.py
@@ -0,0 +1,72 @@
+import os
+
+class LineProgress:
+    """
+    A class used to count the number of lines in each file in a directory.
+
+    ...
+
+    Attributes
+    ----------
+    directory : str
+        a formatted string to print out the directory where the files are located
+
+    Methods
+    -------
+    list_files(directory=None)
+        Counts the number of lines in each file in a directory and returns a dictionary with file paths as keys and line counts as values.
+    """
+    def __init__(self, directory):
+        """
+        Constructs all the necessary attributes for the LineProgress object.
+
+        Parameters
+        ----------
+            directory : str
+                the directory where the files are located
+        """
+        self.directory = directory
+    
+    def list_files(self, directory=None):
+        """
+        Counts the number of lines in each file in a directory and returns a dictionary with file paths as keys and line counts as values.
+
+        If no directory is provided, the method uses the directory passed to the constructor.
+
+        Parameters
+        ----------
+            directory : str, optional
+                the directory where the files are located (default is None which implies the directory passed to the constructor)
+
+        Returns
+        -------
+        dict
+            a dictionary where the keys are the file paths and the values are the line counts
+
+        Example
+        -------
+        >>> line_progress = LineProgress('python')
+        >>> files_dict = line_progress.list_files()
+        >>> print(files_dict)
+        {'python/file1.py': 100, 'python/file2.py': 200, ...}
+        """
+        if directory is None:
+            directory = self.directory
+
+        file_line_count_dict = {}
+        for path in os.listdir(directory):
+            full_path = os.path.join(directory, path)
+            if os.path.isdir(full_path):
+                file_line_count_dict.update(self.list_files(full_path))
+            else:
+                with open(full_path, "r") as file:
+                    line_count = sum(1 for _ in file if _.strip())
+                file_line_count_dict[full_path.replace("\\", "/")] = line_count
+        
+        return file_line_count_dict
+
+
+# line_progress = LineProgress('python')
+# files_dict = line_progress.list_files()
+
+# print(files_dict)
